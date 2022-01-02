5fecb849d114cee6a77b7e31918d8d43d18e4174
Winningtopas <jay07@live.nl>
2022-01-02 18:15:27 +0100

    Added a GenerateMaze script with a function that spawns a cell grid

---
+++ Assets/Scenes/Scripts/Cell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    #region Variables
+    public int x, z;
+    public GameObject cellPrefab;
+    #endregion
+}
